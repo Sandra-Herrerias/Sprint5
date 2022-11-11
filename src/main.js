8379b69b254dd9848872990e52609e53b40279a9
SandraHerrerias <s.herreriasrodriguez@gmail.com>
2022-11-11 18:11:22 +0100

    nivell 1 started

--- src/main.js
+++ src/main.js
@@ -1,1 +1,14 @@
 console.log('HELLO WORLD');
+
+
+const url = 'https://icanhazdadjoke.com/search';
+
+
+
+
+
+async function fetchJokesJSON() {
+    fetch(url)
+        .then(response => response.json())
+        .then(console.log())
+}
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,20 @@
 console.log('HELLO WORLD');
+
+/*
+const url = 'https://icanhazdadjoke.com/search';
+
+let request = new Request(url, {
+    method: 'GET',
+    headers: new Headers({
+      'Content-Type': 'application/json; charset=UTF-8'
+    })
+  });
+  
+  fetch(request)
+  .then((response) => {
+
+    console.log(response.json());
+ 
+    return response.json();
+    
+  })*/
