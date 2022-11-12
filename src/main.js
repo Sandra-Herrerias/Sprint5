22a54af7c7cd078e58d513cde062cfb8f8eceef6
SandraHerrerias <s.herreriasrodriguez@gmail.com>
2022-11-12 20:44:16 +0100

    ex3 changes

--- src/main.js
+++ src/main.js
@@ -1,11 +1,23 @@
 console.log('HELLO WORLD');
-
+class Score {
+    constructor(fjoke, fscore, fdate) {
+        this.joke = fjoke;
+        this.score = fscore;
+        this.date = fdate;
+    }
+}
 
 const API_URL = 'https://icanhazdadjoke.com';
 const HTMLResponse = document.querySelector('#textJokes');
-
+var reportAcudits = [];
+var score;
+var test1;
+var flag = false;
 const renderJokes = async() => {
+
+
     try {
+
         //get response from server
         const respuesta = await fetch(`${API_URL}`, {
             method: 'GET',
@@ -20,31 +32,34 @@ const renderJokes = async() => {
         console.log(data.joke);
         HTMLResponse.innerHTML = `${data.joke}`;
 
+        if (data.joke) {
+            document.getElementById('buttonsScore').style.display = 'block';
+        }
+
+        document.addEventListener('click', function(e) {
+            if (e.target.closest('button')) {
+                e.preventDefault();
+                score = e.target.value;
+                console.log(typeof(e.target.value));
+                console.log(e.target.value);
+            }
+        })
+
+
+        console.log(score);
+
+        test1 = new Score(data.joke, score, new Date());
+
+        reportAcudits.push(test1);
+        console.log(reportAcudits[0]);
+        console.log(reportAcudits);
+        console.log(test1.joke);
+        console.log(test1.score);
+        console.log(reportAcudits);
+
+
     } catch (error) {
         console.log(error);
     }
 
 }
-
-
-
-
-
-/*
-
-
-fetch(`${API_URL}/search`, {
-        method: 'GET', // data can be `string` or {object}!
-        headers: {
-            'Accept': 'application/json',
-            'Accept': 'text/plain',
-            'Accept': 'text/html'
-        }
-    })
-    .then((response) => response.json())
-    .then((jokes) => {
-        const data = jokes.map((joke) => `<li>${joke.joke}</li>`);
-        HTMLResponse.innerHTML = `<ul>${data}</ul>`;
-    })
-    .then(result => console.log('success:', result))
-    .catch(error => console.log('error:', error));*/
